113dab66db21752557e02894ad8543d50adc684c
Anubhav <68240739+AnubhavBangari3@users.noreply.github.com>
2021-04-05 20:15:22 +0530

    Add files via upload

---
+++ Flights/forms.py
@@ -0,0 +1,21 @@
+#for Flights
+from django import forms
+
+from django.contrib.auth.models import *
+
+from django.contrib.auth.forms import UserCreationForm
+
+from .models import *
+
+
+class SignUpForm(UserCreationForm):
+    first_name=forms.CharField(max_length=120,required=False)
+    last_name=forms.CharField(max_length=120,required=False)
+    email=forms.EmailField(max_length=200,required=False)
+
+    class Meta:
+        model=User
+        fields=('username','first_name','last_name','email','password1','password2',)
+
+
+    
